4a355d558688c2765663a3fa94c86d5ba282d653
Simon Teo <teosimon78@gmail.com>
2020-03-25 20:18:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function updateObjectWithKeyAndValue(object, key, value)
 {
-  Object.assign({}, object, {key, value})
+  Object.assign({}, object, {key : value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
