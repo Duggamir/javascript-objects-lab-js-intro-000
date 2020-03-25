36d49aa4bc1290e7045c6fc84f84073748755df5
Simon Teo <teosimon78@gmail.com>
2020-03-25 20:23:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function updateObjectWithKeyAndValue(object, key, value)
 {
-  var newObj = Object.assign({}, object, {key : value});
+  var newObj = Object.assign(newObj, object, {key : value});
   return object;
 }
 
